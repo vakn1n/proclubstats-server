9e4d8f51363e334bd741fbb30c5c8c7c94bca4de
Gil Kagan <gilkag2@gmail.com>
2024-02-16 20:31:04 +0200

    configured typescript, server is running with nodemon

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,8 @@ app.use(json()); // format
 app.use(morgan("dev")); // logger
 dotenv.config(); // set env variables
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server running on port 3000, bitch");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}, bitch`);
 });
