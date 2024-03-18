ab1664cab94c9081abd7beab9d8da08e429a1e19
Gil Kagan <gilkag2@gmail.com>
2024-03-18 21:02:12 +0200

    implemented league table, top scorers, assists and a cache service, need to add top scorers and assits to use the cache

--- src/services/league-service.ts
+++ src/services/league-service.ts
@@ -2,8 +2,10 @@ import { Types } from "mongoose";
 import NotFoundError from "../errors/not-found-error";
 import logger from "../logger";
 import League, { ILeague } from "../models/league";
+import Team from "../models/team";
 import { ITeam } from "../models/team";
-import { IPlayer } from "../models/player";
+import Player, { IPlayer } from "../models/player";
+import CacheService from "./cache-service";
 
 interface LeagueTableRow {
   teamId: string;
@@ -19,10 +21,15 @@ interface LeagueTableRow {
   cleanSheets: number;
 }
 
+const LEAGUE_TABLE_CACHE_KEY = "leagueTable";
+
 class LeagueService {
   private static instance: LeagueService;
+  private cacheService: CacheService;
 
-  private constructor() {}
+  private constructor() {
+    this.cacheService = CacheService.getInstance();
+  }
 
   static getInstance(): LeagueService {
     if (!LeagueService.instance) {
@@ -69,10 +76,42 @@ class LeagueService {
     return await League.find();
   }
 
-  async getLeagueTable(leagueId: string) {
+  async getLeagueTable(leagueId: string): Promise<LeagueTableRow[]> {
+    // look for the table in cache
+    let leagueTable = await this.getLeagueTableFromCache(leagueId);
+    if (!leagueTable) {
+      // Calculate the league table if it's not in the cache
+      leagueTable = await this.calculateLeagueTable(leagueId);
+      await this.setLeagueTableInCache(leagueId, leagueTable);
+    }
+    return leagueTable;
+  }
+
+  private async setLeagueTableInCache(leagueId: string, leagueTable: LeagueTableRow[]): Promise<void> {
+    await this.cacheService.set(`${LEAGUE_TABLE_CACHE_KEY}:${leagueId}`, leagueTable);
+  }
+
+  private async getLeagueTableFromCache(leagueId: string): Promise<LeagueTableRow[] | null> {
+    const leagueTable = await this.cacheService.get(`${LEAGUE_TABLE_CACHE_KEY}:${leagueId}`);
+    if (!leagueTable) return null;
+    return leagueTable as LeagueTableRow[];
+  }
+
+  async updateLeagueTable(leagueId: string) {
+    const leagueTable = await this.calculateLeagueTable(leagueId);
+    await this.setLeagueTableInCache(leagueId, leagueTable);
+  }
+
+  async getTeamPosition(teamId: string, leagueId: string): Promise<number> {
+    const leagueTable = await this.getLeagueTable(leagueId);
+    const position = leagueTable.findIndex((row) => row.teamId === teamId) + 1;
+    return position;
+  }
+
+  private async calculateLeagueTable(leagueId: string): Promise<LeagueTableRow[]> {
     const league = await League.findById(leagueId).populate<{ teams: ITeam[] }>({
       path: "teams",
-      select: "name stats leagueId logoUrl players",
+      select: "name stats",
     });
 
     if (!league) {
@@ -121,15 +160,31 @@ class LeagueService {
       return b.goalsScored - a.goalsScored; // Higher goals scored first
     });
   }
+  async getTopScorers(leagueId: string, limit: number = 10): Promise<IPlayer[]> {
+    const topScorers = await Team.aggregate<IPlayer>([
+      { $match: { leagueId } },
+      { $unwind: "$players" },
+      { $lookup: { from: "players", localField: "players", foreignField: "_id", as: "player" } },
+      { $unwind: "$player" },
+      { $sort: { "player.stats.goals": -1 } },
+      { $limit: limit },
+      { $replaceRoot: { newRoot: "$player" } }, // Replace the root with the player object
+    ]);
 
-  async getTopScorers(limit: number = 10): Promise<IPlayer[]> {
-    // TODO: imp
-    throw new Error("Method not implemented.");
+    return topScorers;
   }
 
-  async getTopAssists(limit: number = 10): Promise<IPlayer[]> {
-    // TODO: imp
-    throw new Error("Method not implemented.");
+  async getTopAssists(leagueId: string, limit: number = 10): Promise<IPlayer[]> {
+    const topAssists = await Team.aggregate<IPlayer>([
+      { $match: { leagueId } },
+      { $unwind: "$players" },
+      { $lookup: { from: "players", localField: "players", foreignField: "_id", as: "player" } },
+      { $unwind: "$player" },
+      { $sort: { "player.stats.assists": -1 } },
+      { $limit: limit },
+    ]);
+
+    return topAssists;
   }
 }
 
