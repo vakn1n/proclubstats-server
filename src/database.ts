1adb06a1db42862cecca0568187aaf1ad7e9d0db
Gil Kagan <gilkag2@gmail.com>
2024-03-22 11:16:35 +0200

    added env variable for username to connect to mongodb

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectToDatabase() {
   try {
-    const connectionString = `mongodb+srv://gilkag2:${process.env.MONGO_DB_PASSWORD}@proclubsstatscluster.4f64qtf.mongodb.net/?retryWrites=true&w=majority`;
+    const connectionString = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@proclubsstatscluster.4f64qtf.mongodb.net/?retryWrites=true&w=majority`;
 
     await mongoose.connect(connectionString);
     console.log("Connected to MongoDB");
