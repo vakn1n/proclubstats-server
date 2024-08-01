98fd9003b281f5df569ba73da30763be02273c9a
Gil Kagan <gilkag2@gmail.com>
2024-08-01 13:47:24 +0300

    added fixture service tests

---
+++ src/mocks/services/mock-game-service.ts
@@ -0,0 +1,15 @@
+import { GameDTO, UpdatePlayerPerformanceDataRequest } from "@pro-clubs-manager/shared-dtos";
+import { Types, ClientSession } from "mongoose";
+import { IGameService } from "../../interfaces/game";
+import { AddGameData, IGame } from "../../models/game";
+
+export class MockGameService implements IGameService {
+  getGameById = jest.fn<Promise<GameDTO>, [string | Types.ObjectId, ClientSession?]>();
+  getGamesByIds = jest.fn<Promise<GameDTO[]>, [string[] | Types.ObjectId[]]>();
+  getCurrentSeasonTeamGames = jest.fn<Promise<GameDTO[]>, [string | Types.ObjectId, number?, ClientSession?]>();
+  createGame = jest.fn<Promise<GameDTO>, [Types.ObjectId, Types.ObjectId, number, AddGameData, ClientSession]>();
+  createFixtureGames = jest.fn<Promise<IGame[]>, [Types.ObjectId, Types.ObjectId, number, AddGameData[], ClientSession]>();
+  updateGameResult = jest.fn<Promise<void>, [string, number, number]>();
+  updateTeamPlayersPerformance = jest.fn<Promise<void>, [string, boolean, UpdatePlayerPerformanceDataRequest[]]>();
+  deleteGame = jest.fn<Promise<void>, [string]>();
+}
